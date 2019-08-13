13bae8fb0b5b8dd2714c9ccf6f69776e57ea81bd
tarasovc47 <tarasovc47@live.ru>
2019-08-13 17:36:03 +0500

    Дбавлен справочник тараскуля, как есть. Только теперь тело страницы и оглавление разнесено по разным соответствующим папкам

---
+++ views/sidebars/taraskul.php
@@ -0,0 +1,48 @@
+<p><br><br></p>
+<li><a href="#taraskul_aup">Административно-управленческий персонал</a></li>
+<li><a href="#taraskul_zam">Заместители директора</a></li>
+<li><a href="#taraskul_mash">Документы, Машбюро</a></li>
+<li><a href="#taraskul_buhi">Бухгалтерская слжуба</a></li>
+<li><a href="#taraskul_peo">Планово-экономический отдел</a></li>
+<li><a href="#taraskul_opr">Отдел планирования и размещения</a></li>
+<li><a href="#taraskul_urist">Юридическая служба</a></li>
+<li><a href="#taraskul_kadry">Отдел кадров</a></li>
+<li><a href="#taraskul_torgi">Отдел по организации торгов и договорной работы</a></li>
+<li><a href="#taraskul_obchepit">Отдел по работе торговых точек и общепита</a></li>
+<li><a href="#taraskul_mathoz">Отдел материально-технического снабжения,складского хозяйства и договорной работы</a></li>
+<li><a href="#taraskul_it">Отдел информационных технологий и телекоммуникаций</a></li>
+<li><a href="#taraskul_hydra">Гидрологическая служба</a></li>
+<li><a href="#taraskul_security">Служба охраны</a></li>
+<li><a href="#taraskul_garage">Гараж</a></li>
+<li><a href="#taraskul_engeener">Инженерная служба</a></li>
+<li><a href="#taraskul_enenergy">Энергетическая служба</li>
+<li><a href="#taraskul_hoz">Хозяйственная служба</a></li>
+<li><a href="#taraskul_med">Медицинская служба</a></li>
+<li><a href="#taraskul_stol">Столовая административного корпуса</a></li>
+<li><a href="#taraskul_uzi">Отделение функциональной и звуковой диагностики</a></li>
+<li><a href="#taraskul_beds">Спальный корпус</a></li>
+<li><a href="#taraskul_med2">Медицинская служба</a></li>
+<li><a href="#taraskul_ovlpimoraiivs">Отделение восстановительного лечения после инфаркта миокарда, операции радиочастотной аблации, имплантации искусственного  водителя сердца</a></li>
+<li><a href="#taraskul_odns">Отделение по долечиванию нестабильной стенокардии
+    </a></li>
+<li><a href="#taraskul_profpatalogiya">Отделение профпатологии</a></li>
+<li><a href="#taraskul_odbalsd">Отделение по долечиванию больных после лечения сахарного диабета</a></li>
+<li><a href="#taraskul_vodogryaz">Водогрязелечебница</a></li>
+<li><a href="#taraskul_loo">Лечебно-оздоровительное отделение</a></li>
+<li><a href="#taraskul_sodbptsm">Специализированное отделение по долечиванию больных после травмы спинного мозга</a></li>
+<li><a href="#taraskul_vgl">Вгл спинального корпуса</a></li>
+<li><a href="#taraskul_ldc">Лечебно-досуговый центр</a></li>
+<li><a href="#taraskul_stol2">Столовая</a></li>
+<li><a href="#taraskul_vodozkom">Водно-оздоровительный комплекс</a></li>
+<li><a href="#taraskul_sport">Спортивно-досуговые службы</a>
+    <ul>
+        <li><a href="#taraskul_culture_mass">Отделение культурно-массового обслуживания</a></li>
+        <li><a href="#taraskul_culture">Культурно-досуговое отделение</a></li>
+        <li><a href="#taraskul_sport2">Спортивное отделение</a></li>
+    </ul>
+</li>
+<li><a href="#taraskul_foodservices">Службы питания</a></li>
+<li><a href="#taraskul_zodiac">Ресторан «зодиак»</a></li>
+<li><a href="#taraskul_hoztech">Хозяйственно-технические службы</a></li>
+<li><a href="#taraskul_other">Прочие</a></li>
+<li><a href="#taraskul_spalnie">Спальные корпуса</a></li>
