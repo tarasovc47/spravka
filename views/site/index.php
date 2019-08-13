0eff971d950355c9032ec66ebbb1145814c12c62
tarasovc47 <tarasovc47@live.ru>
2019-08-13 17:37:19 +0500

    Отрисовка бокового меню вынесена в main.php

--- views/site/index.php
+++ views/site/index.php
@@ -4,20 +4,9 @@
 use app\models\Rkphones;
 
 ?>
-<div class="row">
-    <div class="col-sm-2 col-md-2 my-sidebar">
-        <ul class="nav nav-sidebar">
-            <?php
-            foreach (Rkphones::rkphonesCompanyName() as $item)
-            {
-                echo "<li><a href='#sql_".$item."'> ".$item."</a></li>";
-            }
-            ?>
-        </ul>
-    </div>
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <?php
-            Rkphones::renderTable();
-        ?>
-    </div>
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <?php
+        Rkphones::renderTable();
+    ?>
 </div>
+
