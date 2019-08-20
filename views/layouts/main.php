7bca41b8791ab25b4d28fa8809a4207cddf18c51
tarasovc47 <tarasovc47@live.ru>
2019-08-20 19:03:28 +0500

    закомментировал футер в шаблонах

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -79,14 +79,14 @@ AppAsset::register($this);
         </div>
     </div>
 
-<footer class="footer">
+<!--<footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; My Company <?/*= date('Y') */?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?/*= Yii::powered() */?></p>
     </div>
 </footer>
-
+-->
 <?php $this->endBody() ?>
 </body>
 </html>
