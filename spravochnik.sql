ff43ee1d116e84deb510e0d545e23ee829011feb
tarasovc47 <tarasovc47@live.ru>
2019-09-08 22:30:26 +0500

    добавлю sql с данными

--- spravochnik.sql
+++ spravochnik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 10 2019 г., 14:48
+-- Время создания: Сен 08 2019 г., 20:19
 -- Версия сервера: 5.6.41
 -- Версия PHP: 7.2.10
 
@@ -21,6 +21,59 @@ SET time_zone = "+00:00";
 --
 -- База данных: `spravochnik`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1567101020),
+('m190829_174634_create_taraskulkommutator_table', 1567101035),
+('m190830_152910_create_taraskulinfo_table', 1567179881),
+('m190830_152935_create_taraskulphones_table', 1567179881),
+('m190830_153007_create_taraskulcorpus_table', 1567179881);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `obschie`
+--
+
+CREATE TABLE `obschie` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `image` text,
+  `note` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reestrofcompanies`
+--
+
+CREATE TABLE `reestrofcompanies` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `typeofactivity` text NOT NULL,
+  `numberresource` text NOT NULL,
+  `presencezone` text NOT NULL,
+  `clients` text NOT NULL,
+  `services` text NOT NULL,
+  `colspan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -56,11 +109,96 @@ INSERT INTO `rkphones` (`id`, `department`, `subdepartment`, `position`, `fullna
 (12, 'ООО Русская компания связи', 'Технический отдел', 'Специалист', 'Хабаров Максим Игоревич', '79-66-88<br>\r\n8-982-927-04-51', 'mi.habarov@rk.t72.ru', 'шестидневка'),
 (13, 'ООО Связьпроектсервис', '', 'Главный бухгалтер', 'Барабанова Ирина Владиславовна', '29-50-04', 'barabanova@rk.t72.ru', ''),
 (14, 'ООО Связьпроектсервис', 'Отдел по работе с ФЛ', 'Менеджер', 'Малахова Кристина Ивановна', '68-40-12', 'ki.malahova@rk.t72.ru', 'шестидневка'),
-(15, 'ООО Связьпроектсервис', 'Отдел по работе с ФЛ', 'Менеджер', 'Зырянова Виктория Евгеньевна', '39-01-84', 've.zyrjanova@rk.t72.ru', 'шестидневка');
+(15, 'ООО Связьпроектсервис', 'Отдел по работе с ФЛ', 'Менеджер', 'Зырянова Виктория Евгеньевна', '39-01-84', 've.zyrjanova@rk.t72.ru', 'шестидневка'),
+(16, 'ООО Инфо-Магистраль', '', 'Директор', 'Губин Дмитрий Александрович', '68-00-03', 'gubin@rk.t72.ru', 'пятидневка');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `taraskulcorpus`
+--
+
+CREATE TABLE `taraskulcorpus` (
+  `id` int(11) NOT NULL,
+  `title` text,
+  `floor` int(11) DEFAULT NULL,
+  `room` text,
+  `insidephone` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `taraskulinfo`
+--
+
+CREATE TABLE `taraskulinfo` (
+  `id` int(11) NOT NULL,
+  `title` text,
+  `content` text,
+  `address` text,
+  `phone` text,
+  `email` text,
+  `site` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `taraskulinfo`
+--
+
+INSERT INTO `taraskulinfo` (`id`, `title`, `content`, `address`, `phone`, `email`, `site`) VALUES
+(1, 'Коммутатор \"Тараскуль\"', 'Коммутатор «Тараскуль» – это услуга ручного соединения (выполняется оператором) с внутренними абонентами санатория «Тараскуль».', '', '395-111', '', ''),
+(2, 'Автокоммутатор \"Тараскуль\"', 'Автокоммутатор – реализация автоматического соединения с внутренними абонентами санатория «Тараскуль» путем набора внутреннего номера на клавиатуре телефона.\r\nАбонент может позвонить на любой из этих двух номеров. Если «ручной» коммутатор занят, звонок переадресовывается на автокоммутатор.', '', '395-911', '', ''),
+(3, 'Контакты', '', '625058, г. Тюмень, п. Тараскуль, ул. Санаторная, 10', '8(3452) 39-57-05,<br> 8(3452) 39-57-64, 72-91-69', 'mail@taraskul72.ru', 'http://тараскуль72.рф/'),
+(4, '<!--Справочная информация-->', '<!--Справочная г.Тюмень-->332-332, 75-75-75', '<!--Справочная аэропорта Рощино-->496-450', '<!--Справочная ж/д вокзала-->461-870, 523-609', '<!-Заказ билетов с доставкой-->494-794', ''),
+(5, '<!--Звонки из санатория \"Тараскуль\"-->', '9 (если оплачен)', '8 (если оплачен)', '39-51-11', '39-59-11', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `taraskulphones`
+--
+
+CREATE TABLE `taraskulphones` (
+  `id` int(11) NOT NULL,
+  `department` text,
+  `position` text,
+  `fullname` text,
+  `insidephone` text,
+  `outsidephone` text,
+  `floor` int(11) DEFAULT NULL,
+  `room` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `taraskulphones`
+--
+
+INSERT INTO `taraskulphones` (`id`, `department`, `position`, `fullname`, `insidephone`, `outsidephone`, `floor`, `room`) VALUES
+(1, 'Административно-управленческий персонал', 'Директор', 'Мальцев Сергей Владимирович', '20-05', '39-57-05', NULL, '301'),
+(2, 'Административно-управленческий персонал', 'Приемная директора<br>\r\nСекретарь-машинистка', 'Хрусталева Лариса Ильинична', '20-05', '39-57-05', NULL, '301');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Индексы таблицы `obschie`
+--
+ALTER TABLE `obschie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `reestrofcompanies`
+--
+ALTER TABLE `reestrofcompanies`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `rkphones`
@@ -69,14 +207,62 @@ ALTER TABLE `rkphones`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `taraskulcorpus`
+--
+ALTER TABLE `taraskulcorpus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `taraskulinfo`
+--
+ALTER TABLE `taraskulinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `taraskulphones`
+--
+ALTER TABLE `taraskulphones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `obschie`
+--
+ALTER TABLE `obschie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `reestrofcompanies`
+--
+ALTER TABLE `reestrofcompanies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `rkphones`
 --
 ALTER TABLE `rkphones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT для таблицы `taraskulcorpus`
+--
+ALTER TABLE `taraskulcorpus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `taraskulinfo`
+--
+ALTER TABLE `taraskulinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `taraskulphones`
+--
+ALTER TABLE `taraskulphones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
