23b75bac507e996226397e1caa500ca2a8d377b5
tarasovc47 <tarasovc47@live.ru>
2019-09-22 17:09:03 +0500

    Механизм вывода данных из таблицы taraskulphones готов, необходимо только наполнить таблицу

--- models/Taraskulphones.php
+++ models/Taraskulphones.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\data\Sort;
 use yii\db\Query;
 use yii\helpers\ArrayHelper;
 
@@ -84,6 +85,58 @@ class Taraskulphones extends \yii\db\ActiveRecord
             echo "<ul class='nav nav-sidebar'>
                       <li><a href='#sql_".$item."'> ".$item."</a></li>
                   </ul>";
+        }
+    }
+    public function taraskulphonesQuery()
+    {
+        $taraskulphones = (new Query())
+            ->select('*')
+            ->from('taraskulphones')
+            ->groupBy('department')
+            ->all();
+        return $taraskulphones;
+    }
+    public static function taraskulDepartment()
+    {
+        $company_name = ArrayHelper::getColumn(self::taraskulphonesQuery(),'department');
+        return $company_name;
+    }
+    public static function taraskulphonesRender()
+    {
+        $sort = new Sort([
+            'attributes' => [
+                'department' => [
+                    'asc' => ['department' => SORT_ASC]
+                ]
+            ]
+        ]);
+
+        foreach (self::taraskulDepartment() as $department) {?>
+            <table class="table" id="sql_<?= $department ?>">
+                <thead>
+                <tr><p class="bg-primary"><?= $department ?></p></tr>
+                </thead>
+                <?php
+                $staff = (new Query())
+                    ->select(['department', 'position', 'fullname', 'insidephone', 'outsidephone','room'])
+                    ->from('taraskulphones')
+                    ->where(['department' => $department])
+                    ->orderBy($sort->attributes)
+                    ->all();
+                // В цикле перебираем все записи таблицы и выводим их
+                foreach ($staff as $item) {
+                    ?>
+                    <tr>
+                        <td class="col-md-7"><?= $item['position']."<br>". $item['fullname']?></td>
+                        <td class="col-md-2"><?= $item['insidephone'] ?></td>
+                        <td class="col-md-2"><?= $item['outsidephone'] ?></td>
+                        <td class="col-md-1"><?= $item['room'] ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+            <?php
         }
     }
 }
--- views/site/taraskul_sql.php
+++ views/site/taraskul_sql.php
@@ -4,35 +4,6 @@ use app\models\Taraskulinfo;
 use app\models\Taraskulphones;
 ?>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main taraskul-main">
-    <?php
-/*        foreach (Taraskulinfo::query() as $item) {
-            echo "TITLE ".$item['title']."<br>";
-            echo "CONTENT ".$item['content']."<br>";
-            echo "ADDRESS ".$item['address']."<br>";
-            echo "PHONE ".$item['phone']."<br>";
-            echo "EMAIL ".$item['email']."<br>";
-            echo "SITE ".$item['site']."<br>";
-        }
-
-    */
-    /*foreach (Taraskulinfo::query() as $item) {
-        $taraskulinfoTitle = $item['title'];
-        $taraskulinfoContent = $item['content'];
-        $taraskulinfoAddress = $item['address'];
-        $taraskulinfoPhone = $item['phone'];
-        $taraskulinfoEmail = $item['email'];
-        $taraskulinfoSite = $item['site'];
-    }
-    foreach (Taraskulphones::query() as $item) {
-        $taraskulphonesDepartment = $item['department'];
-        $taraskulphonesPosition = $item['position'];
-        $taraskulphonesFullname = $item['fullname'];
-        $taraskulphonesInsidephone = $item['insidephone'];
-        $taraskulphonesOutsidephone = $item['outsidephone'];
-        $taraskulphonesFloor = $item['floor'];
-        $taraskulphonesRoom = $item['room'];
-    }*/
-?>
     <div class="taraskul-table"> <!--"общий див с данными трёх таблиц"-->
         <div class="taraskul-info"> <!--див с общей информацией-->
             <h4>Общая информация</h4>
@@ -62,6 +33,19 @@ use app\models\Taraskulphones;
                 <?php
                     Taraskulinfo::callingRender();
                 ?>
+            </table>
+            <h3>Персонал санатория</h3>
+            <table class="table">
+                <tr>
+                    <th class="col-md-7">Должность, Ф.И.О.</th>
+                    <th class="col-md-2">Внут. номер</th>
+                    <th class="col-md-2">Город. номер</th>
+                    <th class="col-md-1">Каб.</th>
+                </tr>
+            </table>
+            <?php
+                Taraskulphones::taraskulphonesRender();
+            ?>
             </table>
         </div>
     </div>
